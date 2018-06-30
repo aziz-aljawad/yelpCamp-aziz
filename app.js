1b6d60458dea8fc4a390db306b9b705997e0b250
aziz-aljawad <aziz.aljawad@gmail.com>
2018-06-30 05:05:23 +0300

    version 2.00

--- app.js
+++ app.js
@@ -1,28 +1,62 @@
-var express =require('express');
+
+//dependcies
+var express =require("express");
 var app =express();
 var bodyParser =require("body-parser");
-var campgrounds =
-[
-    {name: "wolf den",image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b0144393f3c17bafeab3_340.jpg"},
-    {name: "lion's rest",image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name: "eagle hills",image: "https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg"},
-    {name: "wolf den",image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b0144393f3c17bafeab3_340.jpg"},
-    {name: "lion's rest",image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name: "eagle hills",image: "https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg"},
-    {name: "wolf den",image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b0144393f3c17bafeab3_340.jpg"},
-    {name: "lion's rest",image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name: "eagle hills",image: "https://farm8.staticflickr.com/7042/7121867321_65b5f46ef1.jpg"}
-];
-app.use(bodyParser.urlencoded({extended:true}));
-app.set('view engine','ejs');
+var mongoose =require("mongoose");
+    mongoose.connect("mongodb://localhost/yelp_camp");
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.set("view engine","ejs");
+
+//SCHEMA SETUP 
+
+var campgroundSchema = new mongoose.Schema(
+    {
+        name:String,
+        image:String,
+        description:String
+    });
+
+var Campground = mongoose.model("Campground",campgroundSchema);
+
+
+// Campground.create(   
+//     {
+//         name: "lion's rest",
+//         image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg",
+//         description: "This camp is natural habitat for many animals around the world weather is cool all year long"
+//     },
+//     function(error,campground)
+//     {
+//         if (error) 
+//         {
+//             console.log(error);    
+//         }
+//         else
+//         {
+//             console.log("Successfully created:- \n"+campground )
+//         }
+//     });
+
+
 app.get('/',function(req,res)
 {
     res.render('landing')
 });
 app.get('/campgrounds',function(req,res)
 {
-
-    res.render('campgrounds',{campgrounds: campgrounds});
+    //get all campground 
+    Campground.find({},function (err,campgrounds)
+    {
+        if (err) 
+        {
+            console.log(err);    
+        }
+        else
+        {
+            res.render('index',{campgrounds: campgrounds});
+        }
+    });
 });
 app.post('/campgrounds',function(req,res)
 {
@@ -30,15 +64,42 @@ app.post('/campgrounds',function(req,res)
     //get data from form and add campgrounds array
     var name = req.body.name;
     var image =req.body.image;
-    var newCampGround ={name:name,image:image}
-    campgrounds.push(newCampGround)
-    //redirect back to campgrounds
-     res.redirect('/campgrounds');
+    var desc =req.body.description;
+    var newCampGround ={name:name,image:image, description: desc }
+    //creat a new campground and save to database
+    Campground.create(newCampGround,function (err,newlycreated)
+    {
+        if (err) 
+        {
+            console.log(err);    
+        }
+        else
+        {
+            //redirect back to campgrounds
+            res.redirect('/campgrounds');
+        }
+    });
 });
 app.get('/campgrounds/new',function(req,res)
 {
     res.render("new.ejs")
 })
+app.get('/campgrounds/:id',function(req,res)
+{
+    Campground.findById(req.params.id,function(err,foundCampground)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("show",{campground :foundCampground });
+            
+        }
+
+    });
+});
 const port = 12345
 app.listen(port,function()
 {
